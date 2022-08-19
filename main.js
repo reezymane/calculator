862edf8ec7f22fd14686645d8ebd2131d326ece4
Deonte McCoy <deontemccoy19@gmail.com>
2022-08-19 13:18:45 -0500

    Create function to store and display number value

--- main.js
+++ main.js
@@ -28,4 +28,19 @@ const operate = function(operator, firstNum, secondNum) {
     }
 };
 
-console.log(operate('add', 8, 4));
+const numbers = document.querySelectorAll('.button1');
+console.log(numbers);
+numbers.forEach((button) => {
+    button.addEventListener('click' , (event) => {
+        let operand1 = event.target.innerHTML;
+        console.log(operand1);
+
+        //Number appears in display
+        const display = document.createElement('div');
+        display.classList.add('display');
+        display.textContent = `${operand1}`;
+
+        let c2 = document.getElementById('container2');
+        c2.appendChild(display);
+    });
+});
